a66a4e3e2d599a33d7d8995521705ed995485e13
ivangoyda <iv.goyda.work@gmail.com>
2024-02-01 13:02:45 +0300

    Add task 2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,7 +10,11 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.List;
+import org.w3c.dom.*;
+import javax.xml.parsers.*;
+import java.io.File;
 
 public class Main {
 
@@ -23,6 +27,12 @@ public class Main {
         writeString(json, "data.json");
 
         System.out.println("Conversion completed successfully!");
+
+        List<Employee> xmlList = parseXML("/Users/ivangoyda/Documents/JavaProject/CsvToJson/src/main/java/org/example/data.xml");
+        String xmlJson = listToJson(xmlList);
+        writeString(xmlJson, "data2.json");
+
+        System.out.println("XML to JSON conversion completed successfully!");
     }
 
     private static List<Employee> parseCSV(String[] columnMapping, String fileName) {
@@ -56,4 +66,43 @@ public class Main {
             e.printStackTrace();
         }
     }
+
+    private static List<Employee> parseXML(String fileName) {
+        try {
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document document = builder.parse(new File(fileName));
+
+            NodeList nodeList = document.getDocumentElement().getChildNodes();
+            List<Employee> employeeList = new ArrayList<>();
+
+            for (int i = 0; i < nodeList.getLength(); i++) {
+                Node node = nodeList.item(i);
+
+                if (node.getNodeType() == Node.ELEMENT_NODE) {
+                    Element element = (Element) node;
+
+                    long id = Long.parseLong(getNodeValue(element, "id"));
+                    String firstName = getNodeValue(element, "firstName");
+                    String lastName = getNodeValue(element, "lastName");
+                    String country = getNodeValue(element, "country");
+                    int age = Integer.parseInt(getNodeValue(element, "age"));
+
+                    employeeList.add(new Employee(id, firstName, lastName, country, age));
+                }
+            }
+
+            return employeeList;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private static String getNodeValue(Element element, String nodeName) {
+        NodeList nodeList = element.getElementsByTagName(nodeName).item(0).getChildNodes();
+        Node node = nodeList.item(0);
+        return node.getNodeValue();
+    }
+
 }
